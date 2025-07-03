1f45a14a3240d04d1e1d6f7d26e1fe2dc370650e
Kgabo0726 <marthadipela@gmail.com>
2025-07-03 23:18:11 +0200

    Add files via upload

---
+++ Python Basics 1/2 python.py
@@ -0,0 +1,8 @@
+#Variable
+
+my_variable = 10
+total_count = 0
+
+#Invalid
+second_variable = 10
+User_name = 20
---
+++ Python Basics 1/6 python.py
@@ -0,0 +1,20 @@
+#Loops
+
+fruits = ["apple" , "banana" , "cherry"]
+
+for fruit in fruits:
+    print(fruit)
+
+
+numbers = [1, 2, 3, 5, 7]
+
+for number in numbers:
+    print(number)
+
+
+#using a while loop to count from 1 to 5
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1 #Increaments the count by 1
